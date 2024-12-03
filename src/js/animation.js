f838739b0391214bdfeb06c47d0673272a3dadad
XKuijpers <x.kuijpers@sintlucasedu.nl>
2024-12-03 16:48:27 +0100

    added api of food data start

--- src/js/animation.js
+++
@@ -1,8 +0,0 @@
-export let rotate = (element, degrees) => {
-    element.style.transform = `rotate(${degrees}deg)`;
-}
-
-export let size = (element, scale ) =>
-    element.onmouseover = () => {
-    element.style.transform = `scale(${scale}%)`;
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
-import {rotate, size} from "./animation.js"
+// The URL of the webservice
+const url = 'https://api.nal.usda.gov/fdc/v1/foods/list?api_key=gH5IylFH1c6TbnFiVMhyy9e4MRmtAY2gg94JJERs&query=gouda';
+const button = document.getElementById("button");
+const foodData = document.getElementById("foodData");
 
-let title = document.getElementById("title");
+const getData = () => {
+    fetch(url)
+        .then((response) => { return response.json() } )
+        .then((data) => { for (let i = 0; i < data.length; i++){
+            // console.log(data[i].description);
+            const p = document.createElement("p");
+            const desc = document.createElement("div");
+            desc.textContent = data[i].description;
+            p.appendChild(desc);
+            foodData.appendChild(desc);
+        }});
+}
 
-rotate(title, 45)
-size(title, 200)
+button.addEventListener("click", function() {
+    getData();
+});
